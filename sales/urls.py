41679ac3b94f40a4300ef2f272a9a188d2e76887
kummitisowmya <sowmyakummiti@gmail.com>
2025-03-12 07:36:44 +0530

    added some new files

--- sales/urls.py
+++ sales/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', sales_dashboard, name='sales_dashboard'),
     path('create-pending-payment/<int:student_id>/', create_pending_payment, name='create_pending_payment'),
 
-    # path('', sales_home, name='sales_home'),
+    path('', sales_home, name='sales_home'),
     path('enroll-student/', enroll_student, name='enroll_student'),
     path('record-payment/<int:student_id>/', record_payment, name='record_payment'),
     path('pending-payments/', view_pending_payments, name='view_pending_payments'),
